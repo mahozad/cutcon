ff255bce51a7a907e8a3d015b4586955de955a7b
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2024-10-27 21:21:27 +0330

    WIP: VLC for linux 2

--- vlc-setup-linux/src/main/resources/script.sh
+++ vlc-setup-linux/src/main/resources/script.sh
@@ -1,6 +1,3 @@
 sudo -S chrpath -r '$ORIGIN' ./usr/lib/libvlc.so
 
-sudo -S chrpath -r '$ORIGIN/..' ./usr/lib/vlc/libvlc_pulse.so.0.0.0
-sudo -S chrpath -r '$ORIGIN/..' ./usr/lib/vlc/libvlc_xcb_events.so.0.0.0
-
 find ./usr/lib/vlc/plugins/ -type f -name "*.so*" | sudo -S xargs -n1 chrpath -r '$ORIGIN/../../..'
