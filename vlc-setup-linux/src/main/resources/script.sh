68d2940a9de0318570fac2e65f5f39041508fa16
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2024-10-28 00:42:53 +0330

    WIP: VLC for linux 2

--- vlc-setup-linux/src/main/resources/script.sh
+++ vlc-setup-linux/src/main/resources/script.sh
@@ -1,6 +1,14 @@
+# Good explanation of rpath/runpath and $ORIGIN:
+# https://unix.stackexchange.com/a/22999
+
 sudo -S apt install chrpath
 
 chrpath -r '$ORIGIN' ./usr/lib/libvlc.so
 
-# Not needed
-# find ./usr/lib/vlc/plugins/ -type f -name "*.so*" | xargs -n1 chrpath -r '$ORIGIN/../../..'
+# Optional step
+# (removing this step does not seem to affect anything but the
+# rpath of the files in plugins/ will be an absolute non-existent path)
+find ./usr/lib/vlc/plugins/ -type f -name "*.so*" | xargs -n1 chrpath -r '$ORIGIN/../../..'
+
+# Exclude libvlc and libvlccore because compressing them seems to break something
+#find ./usr/lib/vlc/plugins/ -name "*.so*" | xargs -n1 ../upx
