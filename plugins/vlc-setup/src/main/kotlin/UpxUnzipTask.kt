ff6b6271d6f6399c438dffae87b58814910c61de
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2024-10-28 14:37:09 +0330

    Refactor some code in vlcSetup plugin

--- plugins/vlc-setup/src/main/kotlin/UpxUnzipTask.kt
+++ plugins/vlc-setup/src/main/kotlin/UpxUnzipTask.kt
@@ -14,14 +14,13 @@ abstract class UpxUnzipTask : Copy() {
     abstract val unzipDirectory: Property<File>
 
     init {
-        from(project.zipTree(zipFile)) {
-            // All the below is to copy only the contents of the root directory
-            // in the archive and not the root directory itself
-            // See https://docs.gradle.org/current/userguide/working_with_files.html#sec:unpacking_archives_example
-            eachFile { it.relativePath = RelativePath(true, *it.relativePath.segments.drop(1).toTypedArray()) }
-            includeEmptyDirs = false // Deletes empty remainder directories
-        }
-        include("**/upx.exe")
+        from(project.zipTree(zipFile))
         into(unzipDirectory)
+        include("**/upx.exe")
+        includeEmptyDirs = false // Deletes empty remainder directories
+        // All the below is to strip the parent directory from the target copy directory
+        // (i.e. only copy the content of the directory and not the directory itself)
+        // See https://docs.gradle.org/current/userguide/working_with_files.html#sec:unpacking_archives_example
+        eachFile { it.relativePath = RelativePath(true, *it.relativePath.segments.drop(1).toTypedArray()) }
     }
 }
