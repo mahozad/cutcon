d938310d1d243bf1f8a64df0f21fda7b3fde45ce
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2024-12-18 11:57:59 +0330

    Add a build utility function to get current OS

--- buildSrc/src/main/kotlin/ir/mahozad/cutcon/BuildUtilities.kt
+++ buildSrc/src/main/kotlin/ir/mahozad/cutcon/BuildUtilities.kt
@@ -7,3 +7,18 @@ fun String?.toBooleanOrNull(): Boolean? {
         else -> return null
     }
 }
+
+enum class OS { WINDOWS, LINUX, MAC, OTHER }
+
+fun getCurrentOs(): OS {
+    val osName = System
+        .getProperty("os.name")
+        .lowercase()
+        .trim()
+    return when {
+        osName.startsWith("win") -> OS.WINDOWS
+        osName.startsWith("mac") -> OS.MAC
+        osName == "linux" -> OS.LINUX
+        else -> OS.OTHER
+    }
+}
--- buildSrc/src/test/kotlin/ir/mahozad/cutcon/BuildUtilitiesTest.kt
+++ buildSrc/src/test/kotlin/ir/mahozad/cutcon/BuildUtilitiesTest.kt
@@ -1,8 +1,15 @@
 package ir.mahozad.cutcon
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import uk.org.webcompere.systemstubs.jupiter.SystemStub
+import uk.org.webcompere.systemstubs.properties.SystemProperties
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension
 
 class BuildUtilitiesTest {
 
@@ -37,6 +44,44 @@ class BuildUtilitiesTest {
             "TrUE" to true,
             " fAlse \t " to false,
             " tRue \t " to true
+        )
+    }
+
+    @Nested
+    @ExtendWith(SystemStubsExtension::class)
+    @TestInstance(PER_CLASS)
+    inner class GetCurrentOsTest {
+
+        @SystemStub
+        val systemProperties = SystemProperties()
+
+        @ParameterizedTest
+        @MethodSource("generateOsNamesAndExpectedResults")
+        fun `When getting current OS, should return proper value`(
+            argument: Pair<String, OS>
+        ) {
+            val (osName, expected) = argument
+            systemProperties.set("os.name", osName)
+            val result = getCurrentOs()
+            assertThat(result).isEqualTo(expected)
+        }
+
+        fun generateOsNamesAndExpectedResults() = listOf(
+            "win" to OS.WINDOWS,
+            "windows" to OS.WINDOWS,
+            "Windows" to OS.WINDOWS,
+            "WINDOWS" to OS.WINDOWS,
+            "Windows 8" to OS.WINDOWS,
+            "Windows XP" to OS.WINDOWS,
+            "windows 10" to OS.WINDOWS,
+            "WINDOWS 10" to OS.WINDOWS,
+            "  wiNDOws  7 " to OS.WINDOWS,
+            "linux" to OS.LINUX,
+            "LINUX" to OS.LINUX,
+            "Mac OS X" to OS.MAC,
+            "mac" to OS.MAC,
+            "abcd" to OS.OTHER,
+            "" to OS.OTHER
         )
     }
 }
