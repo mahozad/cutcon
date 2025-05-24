b5d4ce1c6990060c791b39c6eca303fbe195552e
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2025-05-24 18:52:17 +0330

    Make DefaultVlcDiscoverer only support non-macOS operating systems

--- src/main/kotlin/ir/mahozad/cutcon/component/DefaultVlcDiscoverer.kt
+++ src/main/kotlin/ir/mahozad/cutcon/component/DefaultVlcDiscoverer.kt
@@ -3,6 +3,8 @@ package ir.mahozad.cutcon.component
 import io.github.oshai.kotlinlogging.KotlinLogging.logger
 import ir.mahozad.cutcon.BuildConfig
 import ir.mahozad.cutcon.assetsPath
+import ir.mahozad.cutcon.getCurrentOs
+import ir.mahozad.cutcon.model.OS
 import uk.co.caprica.vlcj.factory.discovery.strategy.NativeDiscoveryStrategy
 import kotlin.io.path.absolutePathString
 import kotlin.io.path.div
@@ -11,9 +13,8 @@ class DefaultVlcDiscoverer : NativeDiscoveryStrategy {
 
     private val logger = logger(name = javaClass.simpleName)
 
-    // See the build script for more information about assetsPath etc.
+    override fun supported() = getCurrentOs() != OS.MAC
     override fun discover() = (assetsPath / BuildConfig.VLC_DIRECTORY_NAME).absolutePathString()
-    override fun supported() = true
     override fun onFound(path: String): Boolean {
         logger.debug { "Found native VLC libraries in $path" }
         return true
